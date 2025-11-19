72d1b89b8fc56cdc06db8314d22f9b1995034b9d
Douglastx7 <douglastx007@gmail.com>
2025-11-18 23:11:33 -0300

    Adicionando sprits e projetos da nave

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Player from "./classes/Player.js";
+import Projectile from "./classes/Projectile.js";
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -6,26 +7,73 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+ctx.imageSmoothingEnabled = false;
+
 const player = new Player(canvas.width,canvas.height);
+const playerProjectiles = [];
 
 const keys = {
     left: false,
     right: false,
+    shoot: {
+        pressed: false,
+        released: true
+    }
+}
+
+const drawProjectiles = () => {
+    playerProjectiles.forEach((Projectile) => {
+        Projectile.draw(ctx);
+        Projectile.update();
+    });
+}
+
+const clearProjectiles = () => {
+     playerProjectiles.forEach((Projectile, index) => {
+        if (Projectile.position.y <= 0) {
+            playerProjectiles.splice(index, 1);
+        }
+     });
 }
  
 const gameLoop = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log(playerProjectiles);
+    drawProjectiles();
+    clearProjectiles();
+
+    ctx.save();
+
+    ctx.translate(
+        player.position.x + player.width / 2,
+        player.position.y + player.height /2
+    )
+
+    if (keys.shoot.pressed && keys.shoot.released) {
+        player.shoot(playerProjectiles);
+        keys.shoot.released = false;
+    }
 
     if(keys.left && player.position.x >= 0) {
        player.moveLeft();
+       ctx.rotate(-0.15)
     }
 
      if(keys.right && player.position.x <= canvas.width - player.width) {
         player.moveRight();
+        ctx.rotate(0.15)
     }
+
+    ctx.translate(
+      -  player.position.x - player.width / 2,
+      -  player.position.y - player.height /2
+    )
 
 
     player.draw(ctx);
+  
+    ctx.restore();
+
     requestAnimationFrame(gameLoop)
 }
 
@@ -35,6 +83,7 @@ addEventListener("keydown", (event) => {
     
     if (key === "a") keys.left = true;
     if (key === "d") keys.right = true;
+    if (key === "enter" || key === " ") keys.shoot.pressed = true;
 });
 
 addEventListener("keyup", (event) => {
@@ -42,6 +91,10 @@ addEventListener("keyup", (event) => {
     
     if (key === "a") keys.left = false;
     if (key === "d")keys.right = false;
+     if (key === "enter" || key === " "){
+         keys.shoot.pressed = false;
+         keys.shoot.released = true;
+        }
 });
 
 gameLoop();
